c460ac6a604dd46604274f2b88f9932b9b42dd0d
Sebastian Paredes <sebaspared0212@gmail.com>
2026-04-27 23:13:20 -0400

    Jest Config Added
    Jest Config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  // Ignore node_modules and vendor folders
+  testPathIgnorePatterns: ["/node_modules/", "/vendor/"],
+  // Collect coverage from the js folder
+  collectCoverageFrom: ["js/**/*.js", "!js/md5.js"],
 };
